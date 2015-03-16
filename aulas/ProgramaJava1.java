d75531b2d902c06a448063ba2983c8796784908d
GabrielMegda <gabrielbazi75@gmail.com>
2015-03-16 11:51:26 -0300

    Create ProgramaJava1.java

---
+++ aulas/ProgramaJava1.java
@@ -0,0 +1,6 @@
+Public class ProgramaJava1{
+  public static void mais(strings [] args){
+  /*imprime no console a frase:'meu primeiro programa java'*/
+  System.out.println("Meu primeiro programa java")
+  }
+}
