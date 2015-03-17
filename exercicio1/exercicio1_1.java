0325b626538a0e1ff8f25419f53e72ffbc67549e
GabrielMegda <gabrielvazi75@gmail.com>
2015-03-17 11:04:59 -0300

    Create exercicio1_1.java

---
+++ exercicio1/exercicio1_1.java
@@ -0,0 +1,18 @@
+package exercicio1_1;
+
+/**
+ *
+ * @author Gabriel Vazi
+ */
+public class Exercicio1_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nome;
+        nome = "Gabriel Megda Vazi";
+        System.out.println("Meu nome é " + nome);
+    }
+    
+}
