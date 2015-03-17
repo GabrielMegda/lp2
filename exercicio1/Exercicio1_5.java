b9a6de078994c80a174de751b2302777aa24f4b8
GabrielMegda <gabrielvazi75@gmail.com>
2015-03-17 11:09:51 -0300

    Create Exercicio1_5.java

---
+++ exercicio1/Exercicio1_5.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package exercicio1_5;
+
+/**
+ *
+ * @author Gabriel Vazi
+ */
+public class Exercicio1_5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double a,b,c,soma,mult,div,sub;
+        a=45;
+        b=20;
+        c=3;
+        soma=a+b+c;
+        mult=a*b*c;
+        div=a/b/c;
+        sub=a-b-c;
+        System.out.println("o valor da soma foi: "+soma);
+        System.out.println("o valor da subtração foi: "+sub);
+        System.out.println("o valor da multiplicação foi: "+mult);
+        System.out.println("o valor da divisão foi:"+div);
+    }
+    
+}
