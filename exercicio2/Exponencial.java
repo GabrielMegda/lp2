d99587c7ca5d1becd91588b1615484ea1b1e0ea9
GabrielMegda <gabrielvazi75@gmail.com>
2015-03-27 16:55:05 -0300

    Create Exponencial.java

---
+++ exercicio2/Exponencial.java
@@ -0,0 +1,20 @@
+package exponencial;
+
+/**
+ *
+ * @author Gabriel Vazi
+ */
+public class Exponencial {
+
+
+    public static void main(String[] args) {
+    
+        System.out.println("# Q C");
+        for (int i = 0; i <= 10; i++) {
+        
+            System.out.println(i +" "+(i*i)+" "+(i*i*i));
+            
+        }
+    }
+    
+}
