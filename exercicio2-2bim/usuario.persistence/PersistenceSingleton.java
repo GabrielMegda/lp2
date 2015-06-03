938aad71cdf09498f50439d0ddd552447690b003
GabrielMegda <gabrielvazi75@gmail.com>
2015-06-02 22:14:51 -0300

    Create PersistenceSingleton.java

---
+++ exercicio2-2bim/usuario.persistence/PersistenceSingleton.java
@@ -0,0 +1,35 @@
+
+package usuario.persistence;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+public class PersistenceSingleton {
+
+    private static final String PERSISTENCEUNIT = "TrabalhoLp2PU";
+    
+    private static PersistenceSingleton instance;
+
+    private EntityManagerFactory emf;
+
+    private PersistenceSingleton() {
+        
+    }
+
+    public static PersistenceSingleton getInstance() {
+        if(instance == null)
+            instance = new PersistenceSingleton();
+        
+        return instance;
+    }
+
+
+    public EntityManagerFactory getEntityManagerFactory() {
+        if(emf == null) {
+            emf = Persistence.createEntityManagerFactory(PERSISTENCEUNIT);
+        }
+       
+        return emf;
+    }
+}
